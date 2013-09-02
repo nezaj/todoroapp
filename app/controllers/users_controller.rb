04c66a72b84511290a279dd100bc9ec9925fd2e6
nezaj <nezaj@server.fake>
2013-09-02 15:41:37 -0700

    Fix bugs due to merging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,7 @@ class UsersController < ApplicationController
 
     if @user.save
       login(@user)
-<<<<<<< HEAD
       redirect_to dashboard_url
-=======
-      render :json => "Success"
->>>>>>> 08ffde3854ab93b4a0b2d6dcb96bfa84b228405f
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new    
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 App::Application.routes.draw do
   resource :session, :only => [:create, :destroy, :new]
-<<<<<<< HEAD
   resource :dashboard, :only => [:show]
-=======
->>>>>>> 08ffde3854ab93b4a0b2d6dcb96bfa84b228405f
   
   resources :users, :only => [:create, :new]
 end
